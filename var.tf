e9a7c7edcdf06125fbfa2af8c80fe1713c5ec596
Lalitha <lalitha.d339@gmail.com>
2023-09-03 18:05:13 +0530

    terraform variables

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "tags" {}
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "elasticache_type" {}
+variable "family" {}
+variable "port"{}
+variable "sg_ingress_cidr" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
