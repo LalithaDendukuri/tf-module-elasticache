84cb6f9f053d3c68e2dbe0358ad42cbb016ad4c7
Lalitha <lalitha.d339@gmail.com>
2023-09-05 05:19:02 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -40,6 +40,6 @@ resource "aws_elasticache_cluster" "example" {
   engine_version       = var.engine_version
   port                 = var.port
   subnet_group_name = aws_elasticache_subnet_group.main.name
-  security_group_ids =aws_security_group.main.name
+  security_group_ids = [aws_security_group.main.id ]
   tags        = merge(local.tags, { Name ="${local.name_prefix}-cluster" })
 }
